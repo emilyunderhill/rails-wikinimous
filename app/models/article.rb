1024d751c0460f453aa69023cfbc7d59da82f9d9
Emily Underhill <emilyru93@gmail.com>
2021-11-09 17:05:27 +0000

    views added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,37 +1,4 @@
 class Article < ApplicationRecord
-  before_action :find_article, only: [:show, :edit, :update, :destroy]
-
-  def index
-    Article.all
-  end
-
-  def show; end
-
-  def new
-    Article.new
-  end
-
-  def create
-    @article = Article.create(article_params)
-  end
-
-  def edit; end
-
-  def update
-    @article.update(article_params)
-  end
-
-  def destroy
-    @article.delete
-  end
-
-  private
-
-  def article_params
-    params.require[:article].permit[:title, :content]
-  end
-
-  def find_article
-    @article = article.find(params[:id])
-  end
+  validates :title, presence: true
+  validates :content, presence: true
 end
